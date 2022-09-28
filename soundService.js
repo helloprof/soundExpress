38f25ca5b44551bff096b3834e2fc4fc5b5c677f
helloprof <suluxan.seneca@gmail.com>
2022-09-28 16:59:03 -0400

    module with promises; initialize to fs read our files from json to objects; getAlbums returns the albums array with objects; getGenres returns the genres array with objects

--- soundService.js
+++ soundService.js
@@ -3,20 +3,48 @@ const fs = require("fs")
 
 
 let albums = []
+let genres = []
 
-module.exports.readMyFile = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                console.log("there's been an error!! ERROR: "+ err)                
-                reject(err)
-            } else {
-                // console.log(data)
-                albums = JSON.parse(data)
-                resolve(albums)
-            }
+module.exports.initialize = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/albums.json', 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        console.log(data)
+        albums = JSON.parse(data)
+        fs.readFile('./data/genres.json', 'utf8', (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            console.log(data)
+            genres = JSON.parse(data)
+            resolve("Success!")
+          }
         })
-    } )
+      }
+    })
+  })
+}
+
+module.exports.getAlbums = () => {
+  return new Promise((resolve, reject) => {
+    if(albums.length > 0) {
+      resolve(albums)
+    } else {
+      reject("no albums")
+    }
+  })
+}
+
+module.exports.getGenres = () => {
+  return new Promise((resolve, reject) => {
+    if(genres.length > 0) {
+      resolve(genres)
+    } else {
+      reject("no albums")
+    }
+  })
 }
 
 
