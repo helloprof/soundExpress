35bfc6315aa697bc768e558e6bae3172c86881c0
helloprof <suluxan.seneca@gmail.com>
2022-09-28 16:57:45 -0400

    server.js with home, albums, and genres routes; static files set up with public; initialize function to start server after files from soundService are read

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
+const express = require("express")
+const app = express()
+const path = require("path")
 
+const soundService = require("./soundService")
 
-const readFile = require("./readFile")
+const HTTP_PORT = 8080
 
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT + " 🚀🚀🚀")
+}
 
-readFile.readMyFile("./data/albums.json").then((albums) => {
-    console.log(albums)
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/about.html"))
+})
+
+app.get("/albums", (req, res) => {
+  soundService.getAlbums().then((albums) => {
+    res.json(albums)
+  }).catch((err) => {
+    console.log(err)
+    res.send("there's been an error!")
+  })
+})
+
+app.get("/genres", (req, res) => {
+  soundService.getGenres().then((genres) => {
+    res.json(genres)
+  }).catch((err) => {
+    console.log(err)
+    res.send("there's been an error!")
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+})
+
+soundService.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart);
 }).catch((err) => {
-    console.log("ERROR: "+ err)
+  console.log(err)
 })
