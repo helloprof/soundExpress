cc3ec268f8e7a728dca0152e6cec5e28b3988f9c
helloprof <suluxan.seneca@gmail.com>
2022-11-09 22:27:35 -0500

    added sequelize connection and sequelize models for album and genre, initialize refactored to seq.sync, album/genre filtering functions use Album.findAll and Genre.findAll, album/genre add functions use Album.create and Genre.create, album/genre delete functions use Album.destroy and Genre.destroy

--- soundService.js
+++ soundService.js
@@ -1,5 +1,52 @@
-
 const fs = require("fs")
+const env = require('dotenv')
+env.config()
+const Sequelize = require("sequelize")
+
+var sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASS, {
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+  },
+  query: { raw: true }
+})
+
+// testing our postgres connection 
+// sequelize
+//     .authenticate()
+//     .then(function() {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(function(err) {
+//         console.log('Unable to connect to the database:', err);
+//     });
+
+// album model
+var Album = sequelize.define('Album', {
+  albumID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true, 
+    autoIncrement: true
+  },
+  title: Sequelize.STRING,
+  artist: Sequelize.STRING,
+  albumCover: Sequelize.STRING,
+  year: Sequelize.INTEGER
+})
+
+// genre model
+var Genre = sequelize.define('Genre', {
+  genreID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true, 
+    autoIncrement: true
+  },
+  genre: Sequelize.STRING,
+})
+
+Album.belongsTo(Genre, {foreignKey: 'genreID'})
 
 
 let albums = []
@@ -7,84 +54,117 @@ let genres = []
 
 module.exports.initialize = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile('./data/albums.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        // console.log(data)
-        albums = JSON.parse(data)
-        fs.readFile('./data/genres.json', 'utf8', (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            // console.log(data)
-            genres = JSON.parse(data)
-            resolve("Success!")
-          }
-        })
-      }
+    sequelize.sync().then(() => {
+      console.log("DATABASE SYNC COMPLETE!")
+      resolve()
+    }).catch((err) => {
+      console.log("DATABASE SYNC FAILED! Error: "+err)
+      reject()
     })
   })
 }
 
 module.exports.getAlbums = () => {
   return new Promise((resolve, reject) => {
-    if(albums.length > 0) {
-      resolve(albums)
-    } else {
-      reject("no albums")
-    }
+    Album.findAll().then((albumData) => {
+      resolve(albumData)
+    }).catch((err) => {
+      console.log("CAN'T FIND ALBUMS! Error: "+err)
+      reject()
+    })
   })
 }
 
 module.exports.getAlbumById = (id) => {
   return new Promise((resolve, reject) => {
-    for(let i = 0; i < albums.length; i++) {
-      var album;
-      if (albums[i].id == id) {
-        album = albums[i]
+    Album.findAll({
+      where: {
+        albumID: id
       }
-    }
-
-    if (album) {
+    }).then((album)=> {
       resolve(album)
-    } else {
-      reject("Album not found!")
-    }
+    }).catch((err) => {
+      console.log("CAN'T FIND ALBUM BY ID! Error: "+err)
+      reject()
+    })
   })
 }
 
 module.exports.getGenres = () => {
   return new Promise((resolve, reject) => {
-    if(genres.length > 0) {
-      resolve(genres)
-    } else {
-      reject("no albums")
-    }
+    Genre.findAll().then((genreData)=> {
+      resolve(genreData)
+    }).catch((err) => {
+      console.log("CAN'T FIND GENRES! Error: "+err)
+      reject()
+    })
+  })
+}
+
+module.exports.addGenre = (genre) => {
+  return new Promise((resolve, reject) => {
+    Genre.create(genre).then(() => {
+      console.log("GENRE CREATED!")
+      resolve()
+    }).catch((err) => {
+      console.log("GENRE CREATION ERROR! Error: "+err)
+    })
   })
 }
 
 module.exports.addAlbum = (album) => {
   return new Promise((resolve, reject) => {
-      album.id = albums.length + 1
-      albums.push(album)
+    Album.create(album).then(() => {
+      console.log("ALBUM CREATED!")
       resolve()
+    }).catch((err) => {
+      console.log("ALBUM CREATION ERROR! Error: "+err)
+    })
   })
 }
 
 module.exports.getAlbumsByGenre = (genreID) => {
   return new Promise((resolve, reject) => {
-    var genreAlbums = []
-    for(let i = 0; i < albums.length; i++) {
-      if (albums[i].genre == genreID) {
-        genreAlbums.push(albums[i])
+    Album.findAll({
+      where: {
+        genreID: genreID
       }
-    }
-    if (genreAlbums) {
-      resolve(genreAlbums)
-    } else {
-      reject("Albums not found for this genre!")
-    }
+    }).then((albums)=> {
+      resolve(albums)
+    }).catch((err) => {
+      console.log("CAN'T FIND ALBUM BY GENRE! Error: "+err)
+      reject()
+    })
+  })
+}
+
+module.exports.deleteAlbum = (albumID) => {
+  return new Promise((resolve, reject) => {
+    Album.destroy({
+      where: {
+        albumID: albumID
+      }
+    }).then(() => {
+      console.log("ALBUM DELETED!")
+      resolve()
+    }).catch((err) => {
+      console.log("ALBUM DELETION ERROR! Error: "+err)
+    })
+  })
+}
+
+module.exports.deleteGenre = (genreID) => {
+  return new Promise((resolve, reject) => {
+    Genre.destroy({
+      where: {
+        genreID: genreID
+      }
+    }).then(() => {
+      console.log("GENRE DELETED!")
+      resolve()
+    }).catch((err) => {
+      console.log("GENRE DELETION ERROR! Error: "+err)
+    })
   })
 }
 
