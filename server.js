22bb24fdaa53551e5bad553f33c9475220a18cee
helloprof <suluxan.seneca@gmail.com>
2022-12-05 14:31:17 -0500

    use client-sessions module, ensureLogin function to check for current session, login form render, and post request for login data (run login function and add user to session if successful), routes for logout and loginHistory

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const streamifier = require('streamifier')
 const env = require('dotenv')
 env.config()
 
+const clientSessions = require("client-sessions");
+
 const soundService = require("./soundService")
 const userService = require("./userService")
 
@@ -31,6 +33,27 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT + " 🚀🚀🚀")
 }
 
+app.use(clientSessions({
+  cookieName: "session", // this is the object name that will be added to 'req'
+  secret: "week10example_web322", // this should be a long un-guessable string.
+  duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+  activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
+}))
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
+app.use(function(req, res, next) {
+  res.locals.session = req.session;
+  next();
+})
+
+
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 
@@ -39,7 +62,7 @@ app.get("/", (req, res) => {
   res.redirect("/albums")
 })
 
-app.get("/albums", (req, res) => {
+app.get("/albums", ensureLogin, (req, res) => {
   if (req.query.genre) {
     soundService.getAlbumsByGenre(req.query.genre).then((genreAlbums) => {
       res.render('albums', {
@@ -58,7 +81,7 @@ app.get("/albums", (req, res) => {
 })
 
 
-app.get("/albums/new", (req, res) => {
+app.get("/albums/new",  ensureLogin, (req, res) => {
   // res.sendFile(path.join(__dirname, "/views/albumForm.html"))
   soundService.getGenres().then((genres) => {
     res.render('albumForm', {
@@ -68,7 +91,7 @@ app.get("/albums/new", (req, res) => {
   })
 })
 
-app.post("/albums/new", upload.single("albumCover"), (req, res) => {
+app.post("/albums/new",  ensureLogin, upload.single("albumCover"), (req, res) => {
   if (req.file) {
     let streamUpload = (req) => {
       return new Promise((resolve, reject) => {
@@ -108,7 +131,7 @@ app.post("/albums/new", upload.single("albumCover"), (req, res) => {
 
 })
 
-app.get("/albums/:id", (req, res) => {
+app.get("/albums/:id",  ensureLogin, (req, res) => {
   soundService.getAlbumById(req.params.id).then((album) => {
     // var array = []
     // array.push(album)
@@ -121,7 +144,7 @@ app.get("/albums/:id", (req, res) => {
   })
 })
 
-app.get("/genres", (req, res) => {
+app.get("/genres",  ensureLogin, (req, res) => {
   soundService.getGenres().then((genres) => {
     res.render('genres', {
       data: genres,
@@ -133,7 +156,7 @@ app.get("/genres", (req, res) => {
   })
 })
 
-app.get("/genres/new", (req, res) => {
+app.get("/genres/new",  ensureLogin, (req, res) => {
   soundService.getGenres().then((genres) => {
     res.render('genreForm', {
       data: genres,
@@ -145,7 +168,7 @@ app.get("/genres/new", (req, res) => {
   })
 })
 
-app.get("/genres/delete/:id", (req, res) => {
+app.get("/genres/delete/:id",  ensureLogin, (req, res) => {
   soundService.deleteGenre(req.params.id).then(() => {
     res.redirect("/genres")
   }).catch((err) => {
@@ -154,7 +177,7 @@ app.get("/genres/delete/:id", (req, res) => {
   })
 })
 
-app.get("/albums/delete/:id", (req, res) => {
+app.get("/albums/delete/:id",  ensureLogin, (req, res) => {
   soundService.deleteAlbum(req.params.id).then(() => {
     res.redirect("/albums")
   }).catch((err) => {
@@ -165,14 +188,14 @@ app.get("/albums/delete/:id", (req, res) => {
 
 // app.use(express.urlencoded({ extended: true }))
 
-app.post("/genres/new", (req, res) => {
+app.post("/genres/new",  ensureLogin, (req, res) => {
   console.log(req.body)
   soundService.addGenre(req.body).then(() => {
     res.redirect("/genres")
   })
 })
 
-app.get("/songs/new", (req, res) => {
+app.get("/songs/new",  ensureLogin, (req, res) => {
   // res.sendFile(path.join(__dirname, "/views/albumForm.html"))
   soundService.getAlbums().then((albums) => {
     res.render('songForm', {
@@ -182,7 +205,7 @@ app.get("/songs/new", (req, res) => {
   })
 })
 
-app.post("/songs/new", upload.single("songFile"), (req, res) => {
+app.post("/songs/new",  ensureLogin, upload.single("songFile"), (req, res) => {
   if (req.file) {
     let streamUpload = (req) => {
       return new Promise((resolve, reject) => {
@@ -226,7 +249,7 @@ app.post("/songs/new", upload.single("songFile"), (req, res) => {
 
 })
 
-app.get("/songs/:albumID", (req, res) => {
+app.get("/songs/:albumID",  ensureLogin, (req, res) => {
   soundService.getSongs(req.params.albumID).then((songs) => {
     res.render('songs', {
       data: songs,
@@ -238,7 +261,7 @@ app.get("/songs/:albumID", (req, res) => {
   })
 })
 
-app.get("/songs/delete/:songID", (req, res) => {
+app.get("/songs/delete/:songID",  ensureLogin, (req, res) => {
   soundService.deleteSong(req.params.songID).then(() => {
     res.redirect("/albums")
   }).catch((err) => {
@@ -266,6 +289,45 @@ app.post("/register", (req, res) => {
       errorMessage: "User Creation Error: "+err
     })
   })
+})
+
+app.get("/login", (req, res) => {
+  res.render('loginForm', {
+    layout: 'main'
+  })
+})
+
+app.post("/login", (req, res) => {
+  // session
+  console.log(req.session.user)
+
+  req.body.userAgent = req.get('User-Agent')
+  userService.loginUser(req.body).then((user) => {
+
+    req.session.user = {
+      username: user.username,
+      email: user.email,
+      loginHistory: user.loginHistory
+    }
+    console.log(req.session.user)
+    res.redirect('/albums')
+  }).catch((err) => {
+    console.log(err)
+    res.render('loginForm', {
+      errorMessage: "User Authentication Error: "+err
+    })
+  })
+})
+
+app.get("/loginHistory", (req, res) => {
+  res.render('loginHistory', {
+    layout: 'main'
+  })
+})
+
+app.get("/logout", ensureLogin, function(req, res) {
+  req.session.reset();
+  res.redirect("/login");
 })
 
 app.use((req, res) => {
